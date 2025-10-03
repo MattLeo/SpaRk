3d85db4ace46a36854016d9c5cd41dd22535f602
MattLeo <matt.leo@outlook.com>
2025-10-03 13:44:51 -0500

    merged core into main repo

---
+++ spark-core/target/debug/deps/spark_core-5a07a1e4ed9a7538.d
@@ -0,0 +1,5 @@
+C:\Users\mtaylor\SpaRk\spark-core\target\debug\deps\spark_core-5a07a1e4ed9a7538.d: src\lib.rs
+
+C:\Users\mtaylor\SpaRk\spark-core\target\debug\deps\libspark_core-5a07a1e4ed9a7538.rmeta: src\lib.rs
+
+src\lib.rs:
---
+++ spark-core/target/debug/deps/spark_core-e7458e7900c39c8e.d
@@ -0,0 +1,5 @@
+C:\Users\mtaylor\SpaRk\spark-core\target\debug\deps\spark_core-e7458e7900c39c8e.d: src\lib.rs
+
+C:\Users\mtaylor\SpaRk\spark-core\target\debug\deps\libspark_core-e7458e7900c39c8e.rmeta: src\lib.rs
+
+src\lib.rs:
