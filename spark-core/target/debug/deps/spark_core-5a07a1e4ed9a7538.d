9a9ed294ef1e7ac630c8e4ff7167d6e1b220e68f
Matt Taylor <matt.leo@outlook.com>
2025-10-14 11:29:00 -0500

    Delete spark-core/target directory

--- spark-core/target/debug/deps/spark_core-5a07a1e4ed9a7538.d
+++
@@ -1,5 +0,0 @@
-C:\Users\mtaylor\SpaRk\spark-core\target\debug\deps\spark_core-5a07a1e4ed9a7538.d: src\lib.rs
-
-C:\Users\mtaylor\SpaRk\spark-core\target\debug\deps\libspark_core-5a07a1e4ed9a7538.rmeta: src\lib.rs
-
-src\lib.rs:
